2ec95db9ed4fdb53eb7b12a6db41f3f95a1851aa
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-12-12 23:46:52 +0300

    feat: config for postgre added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,21 +8,11 @@ import { Company } from './entities/company.entity';
 import { CompanyDetail } from './entities/companydetail.entity';
 import { CompanyModule } from './company/company.module';
 import { CompanyDetailModule } from './companydetail/company-detail.module';
+import { config } from './database/postgresql/config';
 
 @Module({
 	imports: [
-		TypeOrmModule.forRoot({
-			type: 'postgres',
-			url: 'postgres://tywdifrw:d3ZJbXFAvB6gM126AZUEdwltNib6Dzi2@flora.db.elephantsql.com/tywdifrw',
-			// host: 'localhost',
-			// port: 5432,
-			// username: 'user',
-			// password: 'password',
-			// database: 'db',
-
-			entities: [Company, CompanyDetail],
-			synchronize: true,
-		}),
+		TypeOrmModule.forRoot(config),
 		AuthModule,
 		UsersModule,
 		CompanyModule,
